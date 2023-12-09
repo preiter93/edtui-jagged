49997d788c1aabf2373ebe3023e29ba7db9644d1
Philipp Reiter <p.reiter93@gmail.com>
2023-12-09 17:54:45 +0100

    first commit

---
+++ src/jagged/helper.rs
@@ -0,0 +1,115 @@
+use crate::{Index2, Jagged};
+
+impl<T> Jagged<T> {
+    /// Check if a given position is the first row.
+    #[must_use]
+    pub(super) fn is_first_row<I>(&self, index: I) -> bool
+    where
+        I: Into<Index2>,
+    {
+        let index = index.into();
+        index.row == 0 && !self.data.is_empty()
+    }
+
+    /// Check if a given position is the last row.
+    #[must_use]
+    pub(super) fn is_last_row<I>(&self, index: I) -> bool
+    where
+        I: Into<Index2>,
+    {
+        let index = index.into();
+        index.row == self.data.len() - 1 && !self.data.is_empty()
+    }
+
+    /// Check if a given position is the first column.
+    #[must_use]
+    pub(super) fn is_first_col<I>(&self, index: I) -> bool
+    where
+        I: Into<Index2>,
+    {
+        let index = index.into();
+        match self.data.get(index.row) {
+            Some(_) => index.col == 0,
+            None => false,
+        }
+    }
+
+    /// Check if a given position is the last column.
+    #[must_use]
+    pub(super) fn is_last_col<I>(&self, index: I) -> bool
+    where
+        I: Into<Index2>,
+    {
+        let index = index.into();
+        match self.data.get(index.row) {
+            Some(row) => index.col == row.len().saturating_sub(1),
+            None => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    // fn test_first_position() {
+    //     let data: Vec<Vec<i32>> = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+    //     let lines = Jagged2::new(data);
+    //
+    //     assert_eq!(lines.first_index(), Some(Index2::new(0, 0)));
+    // }
+
+    // #[test]
+    // fn test_last_position() {
+    //     let data: Vec<Vec<i32>> = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+    //     let lines = Jagged2::new(data);
+    //
+    //     assert_eq!(lines.last_index(), Some(Index2::new(2, 2)));
+    // }
+
+    #[test]
+    fn test_is_first_col() {
+        let data: Vec<Vec<i32>> = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        let lines = Jagged::new(data);
+
+        assert!(lines.is_first_col(Index2::new(0, 0)));
+        assert!(lines.is_first_col(Index2::new(1, 0)));
+        assert!(!lines.is_first_col(Index2::new(0, 1)));
+    }
+
+    #[test]
+    fn test_is_last_col() {
+        let data: Vec<Vec<i32>> = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        let lines = Jagged::new(data);
+
+        assert!(!lines.is_last_col(Index2::new(0, 0)));
+        assert!(lines.is_last_col(Index2::new(2, 2)));
+    }
+
+    #[test]
+    fn test_is_first_row() {
+        let data: Vec<Vec<i32>> = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        let lines = Jagged::new(data);
+
+        assert!(lines.is_first_row(Index2::new(0, 0)));
+        assert!(!lines.is_first_row(Index2::new(1, 0)));
+    }
+
+    #[test]
+    fn test_is_last_row() {
+        let data: Vec<Vec<i32>> = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        let lines = Jagged::new(data);
+
+        assert!(!lines.is_last_row(Index2::new(0, 0)));
+        assert!(lines.is_last_row(Index2::new(2, 0)));
+    }
+
+    // #[test]
+    // fn test_get_last() {
+    //     let data: Vec<Vec<i32>> = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+    //     let lines = Jagged2::new(data);
+    //
+    //     assert_eq!(lines.get_last(1), Some((&6, Index2::new(1, 2))));
+    // }
+}
---
+++ src/jagged/iter.rs
@@ -0,0 +1,210 @@
+#![allow(clippy::cast_possible_wrap, clippy::cast_sign_loss)]
+use crate::{Index2, Jagged};
+use core::slice::Iter;
+
+pub struct JaggedIterator<'a, T> {
+    pub(super) data: &'a Jagged<T>,
+    pub(super) row: isize,
+    pub(super) col: usize,
+    pub(super) end: Option<Index2>,
+    pub(super) stop: bool,
+}
+
+impl<'a, T> JaggedIterator<'a, T> {
+    /// Instantiates a new [`LinesIterator`] that starts from a given position.
+    #[must_use]
+    pub fn new(data: &'a Jagged<T>) -> Self {
+        Self {
+            data,
+            row: 0,
+            col: 0,
+            end: None,
+            stop: false,
+        }
+    }
+    /// A [`LinesIterator`] that starts from a given position.
+    #[must_use]
+    pub fn from(self, index: Index2) -> Self {
+        Self {
+            data: self.data,
+            row: index.row as isize,
+            col: index.col,
+            end: None,
+            stop: self.stop,
+        }
+    }
+    /// A [`LinesIterator`] that end at a given position.
+    #[must_use]
+    pub fn to(self, index: Index2) -> Self {
+        Self {
+            data: self.data,
+            row: self.row,
+            col: self.col,
+            end: Some(index),
+            stop: self.stop,
+        }
+    }
+}
+
+impl<'a, T> Iterator for JaggedIterator<'a, T> {
+    type Item = (Option<&'a T>, Index2);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current_index = Index2::new(self.row as usize, self.col);
+        if self.stop || current_index.out_of_bounds(self.data) {
+            return None;
+        }
+        if let Some((_, index)) = self.data.next(current_index) {
+            self.row = index.row as isize;
+            self.col = index.col;
+        } else {
+            self.stop = true;
+        }
+        if Some(current_index) == self.end {
+            self.stop = true;
+        }
+        Some((self.data.get(current_index), current_index))
+    }
+}
+
+impl<'a, T> DoubleEndedIterator for JaggedIterator<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.stop || self.row < 0 {
+            return None;
+        }
+        let current_index = Index2::new(self.row as usize, self.col);
+        if let Some((_, index)) = self.data.prev(current_index) {
+            self.row = index.row as isize;
+            self.col = index.col;
+        } else {
+            self.stop = true;
+        }
+        if Some(current_index) == self.end {
+            self.stop = true;
+        }
+        Some((self.data.get(current_index), current_index))
+    }
+}
+
+// impl<'a> DoubleEndedIterator for LinesIterator<'a> {
+//     fn next_back(&mut self) -> Option<Self::Item> {
+//         if self.line < 0 {
+//             return None; // Reached the beginning of the buffer, stop iterating
+//         }
+//         let current = Position::new(self.line as usize, self.column);
+//
+//         if self.column > 0 {
+//             // Move to the previous column within the current line
+//             self.column = self.column.saturating_sub(1);
+//         } else {
+//             // If it's the first column, move to the next line
+//             self.line -= 1;
+//             // Skip empty lines
+//             while self.line >= 0 && self.lines.column_len_at(self.line as usize) == 0 {
+//                 self.line -= 1;
+//             }
+//             self.column = self
+//                 .lines
+//                 .column_len_at(self.line as usize)
+//                 .saturating_sub(1);
+//         }
+//
+//         Some(current)
+//     }
+// }
+
+impl<T> Jagged<T> {
+    /// Returns an iterator that yields the element of a jagged
+    /// array along with its current index.
+    #[must_use]
+    pub fn iter(&self) -> JaggedIterator<'_, T> {
+        JaggedIterator::new(self)
+    }
+
+    /// Returns an iterator over the rows of the jagged array.
+    pub fn iter_row(&self) -> Iter<'_, Vec<T>> {
+        self.data.iter()
+    }
+}
+
+impl<'a, T: Clone> FromIterator<<JaggedIterator<'a, T> as Iterator>::Item> for Jagged<T> {
+    /// Collects the elements from the iterator into a new Jagged array.
+    ///
+    /// Returns a new Jagged array and collects the indices of each element.
+    fn from_iter<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = <JaggedIterator<'a, T> as Iterator>::Item>,
+    {
+        let mut result = Jagged::default();
+        let mut current_row = 0;
+
+        for (value, index) in iter {
+            if result.is_empty() || index.row != current_row {
+                current_row = index.row;
+                result.push(Vec::new());
+            }
+
+            if let Some(value) = value {
+                result.push(value.clone());
+            }
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_obj_long() -> Jagged<char> {
+        Jagged::from("h world!\n\n123.")
+    }
+
+    #[test]
+    fn test_iter() {
+        // given
+        let jagged = test_obj_long();
+        // when
+        let got: Jagged<char> = jagged.iter().collect();
+        //then
+        assert_eq!(got, jagged)
+    }
+
+    #[test]
+    fn test_iter_from() {
+        // given
+        let jagged = test_obj_long();
+        // when
+        let start = Index2::new(0, 3);
+        let got: Jagged<char> = jagged.iter().from(start).collect();
+        //then
+        let exp = Jagged::from("orld!\n\n123.");
+        assert_eq!(got, exp)
+    }
+
+    #[test]
+    fn test_iter_to() {
+        // given
+        let jagged = test_obj_long();
+        // when
+        let start = Index2::new(0, 3);
+        let stop = Index2::new(2, 1);
+        let got: Jagged<char> = jagged.iter().from(start).to(stop).collect();
+        //then
+        let exp = Jagged::from("orld!\n\n12");
+        assert_eq!(got, exp)
+    }
+
+    #[test]
+    fn test_iter_rev() {
+        // given
+        let jagged = test_obj_long();
+        // when
+        let start = Index2::new(2, 4);
+        let got: Jagged<char> = jagged.iter().from(start).rev().collect();
+        //then
+        let exp = Jagged::from(".321\n\n!dlrow h");
+        assert_eq!(got, exp)
+    }
+}
